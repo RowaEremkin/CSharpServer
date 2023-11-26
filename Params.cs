3ed04f8cd4eadc4ffa95f52984b472b18f2d2155
RowaEremkin <49066157+RowaEremkin@users.noreply.github.com>
2023-11-26 13:10:38 +0300

    2

--- Params.cs
+++ Params.cs
@@ -8,7 +8,7 @@
         //Database
         public static string databaseServerIp = "26.149.132.151";
         //public static string databaseServerIp = "localhost";
-        public static string databaseServerUser = "wlad";
-        public static string databaseServerPassword = "&>]hwS(H;bCjX*M!Nu3[jP3rY)";
+        public static string databaseServerUser = "hack";
+        public static string databaseServerPassword = "QWErty0987";
     }
 }
